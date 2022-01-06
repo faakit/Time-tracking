93331038fecae6aec7c0a7825fc16ba9dd99c900
faakit <64740422+faakit@users.noreply.github.com>
2022-01-06 19:24:11 -0300

    Card header improvements: SVGs as background image

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,5 @@
 import styles from './styles.module.scss';
 
-import workSvg from '../../assets/icon-work.svg';
-import playSvg from '../../assets/icon-play.svg';
-import studySvg from '../../assets/icon-study.svg'
-import exerciseSvg from '../../assets/icon-exercise.svg'
-import socialSvg from '../../assets/icon-social.svg'
-import selfCareSvg from '../../assets/icon-self-care.svg'
-
 import ellipsis from '../../assets/icon-ellipsis.svg';
 
 interface cardProps {
@@ -18,14 +11,6 @@ interface cardProps {
 
 
 export function Card(props: cardProps) {
-    const svg: {[key:string]:string} = {
-        "Work": workSvg,
-        "Play": playSvg,
-        "Study": studySvg,
-        "Exercise": exerciseSvg,
-        "Social": socialSvg,
-        "Self Care": selfCareSvg
-    }
     const titleToStyle: {[key:string]:string} = {
         "Work": styles.work,
         "Play": styles.play,
@@ -37,9 +22,7 @@ export function Card(props: cardProps) {
 
     return (
         <div className={styles.card}>
-            <div className={`${styles.header} ${titleToStyle[props.title]}`}>
-                <img src={svg[props.title]} />
-            </div>
+            <div className={`${styles.header} ${titleToStyle[props.title]}`} />
             <div className={styles.cardContent}>
                 <div className={styles.upperContent}>
                     <strong>{props.title[0].toUpperCase() + props.title.substring(1)}</strong>
